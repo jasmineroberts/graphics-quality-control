23d30d96acddf51a37bfdfcde20cc6e1453fca95
Jasmine Roberts <jasminer@mit.edu>
2017-11-02 18:26:06 -0400

    Add files via upload

---
+++ Assets/QualitySettingsMenu/ActivateMenuScriptOnly.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateMenuScriptOnly : MonoBehaviour {
+	public GameObject qualitySettingUI;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Activate_Quality_Menu_In_Game_Button_Clicked()
+	{
+		
+		qualitySettingUI.SetActive (true);
+	}
+	public void De_Activate_Quality_Menu_In_Game_Button_Clicked()
+	{
+
+		qualitySettingUI.SetActive (false);
+	}
+}
